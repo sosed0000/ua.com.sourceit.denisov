0707a9530faaeb9f815462482fe6b79b679fee1e
Oleksandr Denysov <sosed0000@gmail.com>
2023-01-19 23:48:10 +0200

    Task 5.1

--- src/tests/Tests.java
+++ src/tests/Tests.java
@@ -1,22 +1,22 @@
 package tests;
 
+import java.nio.file.Path;
 import java.util.List;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.ConcurrentMap;
 
 public class Tests {
     public static void main(String[] args) {
-        ConcurrentMap<Integer, String> map = new ConcurrentHashMap<>();
-        map.put(1, "sss");
-        map.put(2, "eee");
-        map.put(3, "eee");
-        map.put(4, "eee");
-        map.put(5, "eee");
-        map.remove(7);
-        Integer[] codes = new Integer[map.size()];
-        List<Integer> list = map.keySet().stream().toList();
-        System.out.println(list);
-
+        Path path = Path.of("D:\\233");
+        System.out.println(path);
+        path = Path.of("D:\\233\\");
+        System.out.println(path);
+        path = Path.of("D:/233");
+        System.out.println(path);
+        path = Path.of("D:/233/");
+        System.out.println(path);
+        path = path.resolve("data.txt");
+        System.out.println(path);
 
     }
 
